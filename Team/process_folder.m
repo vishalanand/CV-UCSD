e49d2de868e83dcb5673d46f90e4c85fbff48695
Vishal Anand <anand.vishal2994@gmail.com>
2015-04-14 04:24:40 +0530

    Adding comments on IOU data location

---
+++ Team/process_folder.m
@@ -0,0 +1,28 @@
+rob=[];
+IOU=cell(96,1);
+prob=cell(96,1);
+dirName = 'C:\Users\shweta\Desktop\outData';              %# folder path
+files = dir( fullfile(dirName,'PennPed-bbox-*.txt') );   %# list all *.xyz files
+files = {files.name}';                      %'# file names
+
+data = cell(numel(files),1);                %# store file contents
+for i=1:numel(files)
+    fname = fullfile(dirName,files{i});     %# full path to file
+    data{i} = load(fname);
+    data{i}(:,3)=data{i}(:,3)-data{i}(:,1);
+    data{i}(:,4)=data{i}(:,4)-data{i}(:,2);
+end
+
+H=hpu1;
+for i=1: length(H(:,1))
+    h1=H(i,[4:(4+4*H(i,2)-1)]);
+    A=data{i,1};
+    B=vec2mat(h1,4);
+    IOU{i,1}=rectint(A,B);
+    C=A(:,3).*A(:,4);
+    for j=1:(size(A))(1)
+        prob{i,1}(j,:)=IOU{i,1}(j,:)/C(j);
+    end
+    rob=[rob max(prob{i,:})];
+end
+
