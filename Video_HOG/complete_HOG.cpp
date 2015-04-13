e4c4ab424a1c44d6f5cf0d1d959eede02537cf52
Vishal Anand <anand.vishal2994@gmail.com>
2015-04-14 03:56:50 +0530

    Video HOG code addition

---
+++ Video_HOG/complete_HOG.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[])
+{
+	VideoCapture cap(argv[1]);
+	VideoCapture sequence(argv[1]);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 240);
+	//if(!cap.isOpened())
+	if(!sequence.isOpened())
+	{
+		cout<<"\nCould not open video file.\n";
+		return -1;
+	}
+
+	Mat img;
+	HOGDescriptor hog;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+	namedWindow("video capture", CV_WINDOW_AUTOSIZE);
+	while(true)
+	{
+		cap >> img;
+		if(!img.data)
+			continue;
+
+		vector<Rect> found, found_filtered;
+		vector<double> confidence;
+		hog.detectMultiScale(img, found, confidence, 0, Size(8,8), Size(32,32), 1.05, 2);
+
+		size_t i, j;
+		for(i=0; i<found.size(); i++)
+		{
+			Rect r = found[i];
+			for(j=0; j<found.size(); j++)
+				if(j!=i && (r & found[j])==r)
+					break;
+			if(j==found.size())
+				found_filtered.push_back(r);
+		}
+		for(i=0; i<found_filtered.size(); i++)
+		{
+			Rect r = found_filtered[i];
+			r.x += cvRound(r.width*0.1);
+			r.width += cvRound(r.width*0.1);
+			r.y += cvRound(r.width*0.1);
+			r.height += cvRound(r.width*0.1);
+			rectangle(img, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
+			int x=r.x + r.width/3, y=r.y + r.height/3;
+			//int x=(r.x + r.height)/2, y=(r.y + r.width)/2;
+			ostringstream strs;
+			strs << confidence[i];
+			string str_value = strs.str();
+			putText(img, str_value, Point(x, y), FONT_HERSHEY_SIMPLEX, .5, Scalar(255, 100, 0), 2);
+			cout<<confidence[i]<<" is one of the confidences\n";
+		}
+		imshow("video capture", img);
+		int c = waitKey(30) & 255;
+		if( c == 'q' || c == 'Q')
+			break;
+	}
+	return 0;
+}
